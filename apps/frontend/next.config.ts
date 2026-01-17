be44092a5613ee20838bc1b05716e9244731996b
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2026-01-17 10:05:30 +0530

    restart

--- apps/frontend/next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  transpilePackages: ["@repo/ui", "@repo/common"],
-  /* config options here */
-};
-
-export default nextConfig;
